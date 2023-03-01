ee36f06260a8215eb8a955ee5daf9866f6b9f215
kirsy05 <88324002+kirsy05@users.noreply.github.com>
2023-03-01 16:23:45 +0100

    add libaries und first lines of code

--- sketch_mar1b/sketch_mar1b.ino
+++ sketch_mar1b/sketch_mar1b.ino
@@ -2,9 +2,32 @@
 Author: Kirsten Schulze
 eMail: kirsten.schulze@studium.uni-hamburg.de
 Date: 2023-03-01
-/*****
+
+This script uses an Arduino together with a 0518
+*****/
+
+#include <SD.h>
+#include <SPI.h>
+#include <RTClib.h>
+#include <OneWire.h>
+
+RTC_051307 rtc;
+
+OneWire ow(4);
 
 void setup() {
+  Serial.begin(9600); 
+
+  if(!rtc.begin()) {
+    Serial.println("RTC is NOT running. Let's set the time now!");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+
+  if(!SD.begin(10)) {
+    Serial.println("SD module initialization failed or Card is not present");
+    return;
+  }
+
   // put your setup code here, to run once:
 
 }
